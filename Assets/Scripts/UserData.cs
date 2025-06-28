5014455c970603b9455479183433c5d57f7d56a7
DaegalMagic <chanhee0424@naver.com>
2025-06-28 22:09:27 +0900

    Add final scripts (UserData, UserDataLoader) with meta files

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,20 @@
+﻿public class UserData
+{
+	int[,] field;
+
+}
+
+public struct Resource {
+	float red;
+	float green;
+	float blue;
+	float yellow;
+	float garo;
+	float sero;
+	float daegak;
+	float bomb;
+}
+
+public struct Item {
+
+}
